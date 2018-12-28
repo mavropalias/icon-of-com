003024c68d0d902afc6b1296aa937d39f5166732
Kostas Mavropalias <mavropalias@gmail.com>
2018-12-28 11:29:01 +0000

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-749229-5'
       }
     },
     {
