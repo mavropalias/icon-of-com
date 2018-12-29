af1aaa924999d2d7438c9a37b48b9d56fcf51273
Kostas Mavropalias <mavropalias@gmail.com>
2018-12-29 09:13:43 +0000

    Fix link

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import SEO from '../components/seo'
 
@@ -6,7 +7,9 @@ const NotFoundPage = () => (
   <div>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that does not exist... [go home](/).</p>
+    <p>
+      You just hit a route that does not exist... <Link to="/">go home</Link>.
+    </p>
   </div>
 )
 
