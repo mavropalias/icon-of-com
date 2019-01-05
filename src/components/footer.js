21a338eb1d32513356838aec50524da70a43ec09
Kostas Mavropalias <mavropalias@gmail.com>
2019-01-05 10:50:09 +0000

    Add source link

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,12 @@ import { colors, rhythm } from '../utils/typography'
 const Footer = () => (
   <StyledFooter>
     <Navigation compact={true} />
-    <Copyright>&copy; Kostas Mavropalias</Copyright>
+    <Copyright>
+      <div>&copy; Kostas Mavropalias.</div>
+      <SourceLink href="https://github.com/mavropalias/icon-of-com">
+        Source
+      </SourceLink>
+    </Copyright>
   </StyledFooter>
 )
 
@@ -22,6 +27,13 @@ const Copyright = styled.small`
   color: ${colors.secondary};
   font-size: 14px;
   line-height: 14px;
+`
+
+const SourceLink = styled.a`
+  margin-top: 16px;
+  color: ${colors.secondary};
+  display: inline-block;
+  font-size: 11px;
 `
 
 export default Footer
